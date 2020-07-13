85ee6417d409b46ae293eb7dd1d98696631b9b14
mramsterdam85 <hunggs.no7@gmail.com>
2020-07-13 22:26:07 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,43 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Clousre part 1</h1>`;
+
+function isPrimeCache(v) {
+  return new function () {
+    if (v <= 3) {
+      return v > 1;
+    }
+    if (v % 2 == 0 || v % 3 == 0) {
+      return false;
+    }
+    var vSqrt = Math.sqrt(v);
+    for (let i = 5; i <= vSqrt; i += 6) {
+      if (v % i == 0 || v % (i + 2) == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+var dict = {};
+function factorize(v) {
+  var isPrime = dict[v];
+  if(!isPrime) {
+    isPrime = isPrimeCache(v);
+    dict[v] = isPrime;
+  }
+  console.log('isPrime ' + v + ':' + isPrime(v));
+  if (!isPrime(v)) {
+    let i = Math.floor(Math.sqrt(v));
+    while (v % i != 0) {
+      i--;
+    }
+    return [
+      ...factorize(i),
+      ...factorize(v / i)
+    ];
+  }
+  return [v];
+}
