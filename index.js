84d086c8937f60c469a9f78a4e99352a50d1ce8f
mramsterdam85 <hunggs.no7@gmail.com>
2020-07-20 23:03:44 +0200

    1 file modified

--- index.js
+++ index.js
@@ -5,42 +5,44 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Clousre part 1</h1>`;
 
-function isPrimeCache(v) {
-  return function () {
+// function isPrime(v) {
+//     if (v <= 3) {
+//         return v > 1;
+//     }
+//     if (v % 2 == 0 || v % 3 == 0) {
+//         return false;
+//     }
+//     var vSqrt = Math.sqrt(v);
+//     for (let i = 5; i <= vSqrt; i += 6) {
+//         if (v % i == 0 || v % (i + 2) == 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+var isPrimeCache = (function isPrime1(v) {
+  var primes = {};
+  return function isPrime1(v) {
+    if(v in primes) {
+      console.log('From cache...');
+      return primes[v];
+    }
     if (v <= 3) {
-      return v > 1;
+        return (primes[v] = v > 1);
     }
     if (v % 2 == 0 || v % 3 == 0) {
-      return false;
+        return (primes[v] = false);
     }
     var vSqrt = Math.sqrt(v);
     for (let i = 5; i <= vSqrt; i += 6) {
-      if (v % i == 0 || v % (i + 2) == 0) {
-        return false;
-      }
+        if (v % i == 0 || v % (i + 2) == 0) {
+            return (primes[v] = false);
+        }
     }
-    return true;
-  }
-}
+    return (primes[v] = true);
+  };
+})();
 
-var dict = {};
-function factorize(v) {
-  var isPrime = dict[v];
-  if(!isPrime) {
-    isPrime = isPrimeCache(v);
-    dict[v] = isPrime;
-  }
-  if (!isPrime(v)) {
-    let i = Math.floor(Math.sqrt(v));
-    while (v % i != 0) {
-      i--;
-    }
-    return [
-      ...factorize(i),
-      ...factorize(v / i)
-    ];
-  }
-  return [v];
-}
-
-console.log(factorize(120));
+console.log(isPrimeCache(300));
+console.log(isPrimeCache(300));
